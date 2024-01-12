fc4d44293e6fa4a75f7cae547dc9cb1dff9fc890
vishwajeetverma-in <vishwa11126@gmail.com>
2024-01-12 18:31:38 +0530

    random generation of platforms using coroutine | using simple concept of adding the transform of  last platform

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject platformPrefab;
+    public Transform lastPlatform;
+    Vector3 lastPosition;
+    Vector3 newPos;
+    bool stop;
+    private void Start()
     {
+        lastPosition = lastPlatform.position;
+        StartCoroutine(SpawnPlatforms());
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+      
+    }
+
+    IEnumerator SpawnPlatforms()
+    {
+        while (!stop)
+        {
+            GeneratePosition();
+            Instantiate(platformPrefab, newPos, Quaternion.identity);
+            lastPosition = newPos;
+            yield return new WaitForSeconds(0.1f);
+        }
+      
+    }
+
+    
+
+    void GeneratePosition()
+    {
+        newPos = lastPosition;
+        int rand = Random.Range(0, 2);
+        if (rand > 0)
+        {
+            newPos.x += 2f;
+        }
+        else
+        {
+            newPos.z += 2f;
+        }
     }
 }
