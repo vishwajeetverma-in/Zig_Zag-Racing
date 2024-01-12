7329fe6b996a7bea8fbd169c0f18ce9e2a606f39
vishwajeetverma-in <vishwa11126@gmail.com>
2024-01-12 14:33:25 +0530

    flagging used 4 times in code to oscillate direction of car between 2 direction (transform.rotation of car initially and 90 clockwise)

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float moveSpeed;
+    bool movingLeft = true;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        Move();
+        CheckInput();
+    }
+
+    void Move()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+
+    private void CheckInput()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeDirection();
+        }
+    }
+
+    void ChangeDirection()
+    {
+        if (movingLeft)
+        {
+            movingLeft = false;
+            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+
+        }
+
+    else
+        {
+            movingLeft = true;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+    }
+
+}
